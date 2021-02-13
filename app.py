483f43b432156b04658e59f27bd3613b67431e40
liorbuddha <51681260+liorbuddha@users.noreply.github.com>
2021-02-13 21:17:02 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from time import sleep
 from binance.client import Client
 from binance.enums import *
 import config
-##-------CREATED BY LIOR BEN-ELIEZER-----------
 
+## NEED TO ADD PROFIT
 
 # VARIABLES
 #  sym - symbol of trading
@@ -21,14 +21,21 @@ client = Client(config.key1, config.key2)
 
 # Balance, symb is the symbol of wallet you want to check
 symb = 'BNB'
-balance = config.SetQty(symb)
 # set amount of trading coin (if trading BNBUSDT, take amount of BNB for buy and sell)
+balance = config.SetQty(symb)
 
 # GET INITIAL PRICE
 # for fixed price, insert number
 startPrice = config.lastTrade(sym)
 print('start price is %s' % startPrice)
-sleep(3)
+total = 0
+x = 0
+y = 0
+z = 0
+
+# price tracker
+uphight = startPrice
+
 while True:  # -----  MAIN LOOP --------
 
     # Get corrent price
@@ -36,57 +43,61 @@ while True:  # -----  MAIN LOOP --------
     print("corrent price is %s" % correntPrice)  # print(f"corrent price is {correctPrice}")
 
     while not trade:  # -------- WAITING TO BUY-------
-        sleep(2)
+        sleep(1)
         correntPrice = config.SetCorrrent(sym)  # Corrent
         # GOES UP
-        if correntPrice - startPrice >= 0:
-            print(f"waiting for price to go down ", correntPrice, "bought", bought, "sold ", sold)
-            if correntPrice - startPrice >= 10:  # ---------------STEPPING UP
-                bought += 1
-                # --------SET ORDER TO BUY--------
-                config.Trade.buy(sym, 0.48)
-                print('B %s' % bought)
-                sleep(1)
-                startPrice = config.lastTrade(sym)
-                print("BOUGHT!!!!! %s" % startPrice)
-                trade = True
+        if correntPrice - startPrice >= 0:  # sold and price went up
+            print(f"waiting for price to go down ", correntPrice, "bought", bought, "sold ", sold, "total ", total)
         else:
-            if correntPrice - startPrice <= -0.012*startPrice:
-                bought += 1
-                # --------SET ORDER TO BUY--------
-                config.Trade.buy(sym, 0.4)
-                print('B %s' % bought)
-                sleep(1)
-                startPrice = config.lastTrade(sym)
-                print("BOUGHT!!!!! %s" % startPrice)
-                trade = True
-
-            else:
-                print("waiting for price to go down ", correntPrice, "bought", bought, "sold ", sold)
+            while (correntPrice - startPrice <= -0.018 * startPrice) and (trade == False):
+                correntPrice = config.SetCorrrent(sym)
+                uphight = correntPrice
+                if correntPrice - uphight < 0:
+                    print('GOING DOWN!! ', correntPrice, "bought", bought, "sold ", sold, "total ", total)
+                    startPrice = uphight
+                    print('new starting price: ', startPrice)
+                else:
+                    if correntPrice - uphight < 0.01 * uphight:
+                        bought += 1
+                        # --------SET ORDER TO BUY--------
+                        config.Trade.buy(sym, 0.41)
+                        print("******************************************************")
+                        print('B %s' % bought)
+                        sleep(1)
+                        startPrice = config.lastTrade(sym)
+                        print("BOUGHT!!!!! ", startPrice)
+                        print("******************************************************")
+                        trade = True
+            print("waiting for price to go down ", correntPrice, "bought", bought, "sold ", sold, "total ", total)
 
     while trade:  # ------ WAITING FOR SELL--------
-        sleep(2)
+        sleep(1)
         correntPrice = config.SetCorrrent(sym)  # corrent
-        if correntPrice <= startPrice:
-            print("waiting for price to go up ", correntPrice, "bought", bought, "sold ", sold)
-            if correntPrice - startPrice <= -10:  # ----------STEPPING DOWN
-                sold += 1
-                # --------SET ORDER TO SELL--------
-                config.Trade.sell(sym, 0.4)
-                print('S %s' % sold)
-                sleep(1)
-                startPrice = config.lastTrade(sym)
-                print("SOLD!!!!! %s" % startPrice)
-                trade = False
-        else:
-            if correntPrice - startPrice >= 0.03*startPrice:
-                sold += 1
-                # --------SET ORDER TO SELL--------
-                config.Trade.sell(sym, 0.5)
-                print('S %s' % sold)
-                sleep()
-                startPrice = config.lastTrade(sym)
-                print("SOLD!!!!! %s" % startPrice)
-                trade = False
-            else:
-                print("waiting for price to go up ", correntPrice, "bought", bought, "sold ", sold)
+        if correntPrice <= startPrice:  # bought and price went down
+            print("waiting for price to go up ", correntPrice, "bought", bought, "sold ", sold, "total ", total)
+
+        else:  # bought and price went up
+            uphight = correntPrice
+            while (correntPrice - startPrice >= 0.03 * startPrice) and (trade == True):
+                correntPrice = config.SetCorrrent(sym)
+                if correntPrice - uphight > 0:
+                    print('GOING UP!! ', correntPrice, "bought", bought, "sold ", sold, "total ", total)
+                    startPrice = uphight
+                    print('new starting price: ', startPrice)
+                else:
+                    if correntPrice - uphight < -0.001 * uphight:
+                        sold += 1
+                        # --------SET ORDER TO SELL--------
+                        config.Trade.sell(sym, 0.41)
+                        print("******************************************************")
+                        print('S %s' % sold)
+                        sleep(1)
+                        startPrice = config.lastTrade(sym)
+                        print("SOLD!!!!! ", startPrice)
+                        print("******************************************************")
+                        x = config.total(sym, 1)
+                        y = config.total(sym, 2)
+                        z = x - y
+                        total += z
+                        trade = False
+            print("waiting for price to go up ", correntPrice, "bought", bought, "sold ", sold, "total ", total)
--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from binance.client import Client
 from binance.enums import *
 
 # API KEY, get from your account on binance
-key1 = "yourApiKey"
-key2 = "yourSecurityKey"
+key1 = "WvrBN4oGcbJzFmcJAMlj7vqKcuFBida2U9M0RqprF1z5cdO8caZ2F0DQMzFSMtNu"
+key2 = "BMlog6nC1hldS4YQcAlujVB5zFehkGjw9gffc1T77FjPqcJAmetiJeqvGyYpVDOo"
 client = Client(key1, key2)
 
 
@@ -21,9 +21,14 @@ def SetQty(sym):
     balance = client.get_asset_balance(asset=sym)
     balance = balance['free']
     balance = float(balance)
+    balance *= 100
+    balance = int(balance)
+    balance = float(balance)
+    balance /= 100
     return balance
 
 
+# set price of last trade
 def lastTrade(sym):
     trades = client.get_my_trades(symbol=sym)
     trades = trades[-1]
@@ -31,11 +36,23 @@ def lastTrade(sym):
     trades = float(trades)
     return trades
 
+
+# indicate is buyer or seller
 def isBuyer(sym):
     buyer = client.get_my_trades(symbol=sym)
     buyer = buyer[-1]
     buyer = buyer['isBuyer']
     return buyer
+
+
+# returns sub between trade[-1] and trade[-2]
+def total(sym, i):
+    totalTrade = client.get_my_trades(symbol=sym)
+    totalTrade = totalTrade[-1*i]
+    totalTrade = totalTrade['quoteQty']
+    totalTrade = float(totalTrade)
+    return totalTrade
+
 
 class Trade:
     def sell(sym, qty):
